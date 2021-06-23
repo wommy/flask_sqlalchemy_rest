9b90e1aeb29c255019637d8572bd5ecbff6c87ff
wommy <tom@technomad.media>
2021-06-22 20:48:44 -0400

    updated

--- app.py
+++ app.py
@@ -14,27 +14,74 @@ db = SQLAlchemy(app)
 # init ma
 ma = Marshmallow(app)
 
-# product class/model
-class Product(db.Model):
+# quantity class/model
+class Quantity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
-    description = db.Column(db.String(200))
-    price = db.Column(db.Float)
-    qty = db.Column(db.Integer)
-
-    def __init__(self, name, description, price, qty):
+    qty = db.Column(db.Integer, default=0)
+    def __init__(self, name, qty):
         self.name = name
-        self.description = description
-        self.price = price
         self.qty = qty
 
-# product schema
-class ProductSchema(ma.Schema):
+# quantities schema
+class QuantitySchema(ma.Schema):
     class Meta:
-        fields = ('id','name','description','price','qty')
+        fields = ('id','name','qty')
+quantity_schema = QuantitySchema()
+# products _schema = ProductSchema(many=True)
 
-product_schema = ProductSchema()
-products_schema = ProductSchema(many=True)
+# todo class/model
+# class Todo(db.Model):
+    # id = db.Column(db.Integer, primary_key=True)
+    # name = db.Column(db.String(100), unique=True)
+    # completed = db.Column(db.Boolean, default=False)
+    # def __init__(self, name, completed):
+        # self.name = name
+        # self.completed = completed
+
+# todo schema
+# class TodoSchema(ma.Schema):
+    # class Meta:
+        # fields = ('id','name','completed')
+# todo_schema = TodoSchema()
+# products _schema = ProductSchema(many=True)
+
+# log class/model
+# class Log(db.Model):
+    # id = db.Column(db.Integer, primary_key=True)
+    # name = db.Column(db.String(100), unique=True)
+    # published = db.Column(db.Boolean, default=True)
+    # TODO default datetime.Now()
+    # createdAt = db.Column(db.DateTime)
+    # def __init__(self, name, published, createdAt):
+        # self.name = name
+        # self.published = published
+        # self.createdAt = createdAt
+
+# log schema
+# class LogSchema(ma.Schema):
+    # class Meta:
+        # fields = ('id','name','published','createdAt')
+# log_schema = LogSchema()
+# products _schema = ProductSchema(many=True)
+
+# productMaterialsDetails class/model
+# class PMDetails(db.Model):
+    # id = db.Column(db.Integer, primary_key=True)
+    # productId = db.Column(db.Integer, ForeignKey(Quantity.id))
+    # materialId = db.Column(db.Integer, ForeignKey(Quantity.id))
+    # product_id = db.Column(db.Integer, db.ForeignKey('quantity.id'), nullable=False)
+    # material_id = db.relationship('Quantity', backref=db.backref('product', lazy=True))
+    # def __init__(self, productId, materialId):
+        # self.productId = productId
+        # self.materialId = materialId
+
+# productMaterialDetails schema
+# class PMDetailsSchema(ma.Schema):
+    # class Meta:
+        # fields = ('id','productId','materialId')
+# pmdetails_schema = PMDetailsSchema()
+#products_schema = ProductSchema(many=True)
 
 # create product
 @app.route('/product', methods=['POST'])
